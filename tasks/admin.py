d5f8989949663b18f453187d41581948cff15a6d
altvec <me@sergeykalistratov.com>
2020-02-05 17:06:50 +0500

    Added initial tasks model

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,3 +3,7 @@
 from django.contrib import admin  # noqa: F401
 
 # Register your models here.
+from .models import Task, Tag
+
+admin.site.register(Task)
+admin.site.register(Tag)
--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models  # noqa: F401
+from users.models import CustomUser
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    STATUSES = (
+        ('new', 'New task'),
+        ('in_progress', 'In progress'),
+        ('testing', 'In testing'),
+        ('completed', 'Completed'),
+    )
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=512)
+    tags = models.ManyToManyField(Tag, related_name='tasks')
+    creator = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='tasks_created_by',
+    )
+    assigned_to = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='tasks_assigned_to',
+    )
+    status = models.CharField(max_length=20, choices=STATUSES, default='new')
+
+    def __str__(self):
+        return self.name
