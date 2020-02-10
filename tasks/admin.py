9bba1b9b551168459351af0929aca590d4ba2e57
altvec <me@sergeykalistratov.com>
2020-02-10 22:35:44 +0500

    Improved tasks display in admin interface

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,5 +4,12 @@ from django.contrib import admin
 
 from tasks.models import Tag, Task
 
+
+class TaskAdmin(admin.ModelAdmin):
+    """Display tasks in admin interface."""
+
+    list_display = ('name', 'description', 'status', 'creator', 'assigned_to')
+
+
 admin.site.register(Tag)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
