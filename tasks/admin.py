711beadd8478f97cde71dddf1679ca4bff4ce587
altvec <me@sergeykalistratov.com>
2020-02-20 08:50:53 +0500

    Add search and filter fields for taskstatus in admin interface

--- tasks/admin.py
+++ tasks/admin.py
@@ -10,6 +10,8 @@ class TaskAdmin(admin.ModelAdmin):
     """Display tasks in admin interface."""
 
     list_display = ('name', 'description', 'status', 'creator', 'assigned_to')
+    list_filter = ('status',)
+    search_fileds = ('name', 'description')
 
 
 admin.site.register(Tag)
