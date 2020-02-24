a47ef40f4e4293302a8971a0674498fea0317d99
altvec <me@sergeykalistratov.com>
2020-02-24 13:58:23 +0500

    Automatically login user after successful registration

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
 
 from users.forms import UserCreationForm
@@ -7,11 +9,18 @@ from users.forms import UserCreationForm
 
 def register_user(request):
     """User register function."""
+    form = UserCreationForm(request.POST or None)
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+            messages.info = (  # noqa: WPS110
+                request,
+                "Thanks for registering. You're now logged in.",
+            )
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+            )
+            login(request, user)
+            return redirect('/')
+    return render(request, 'registration/register.html', {'form': form})
